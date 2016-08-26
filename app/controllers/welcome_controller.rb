a0d88ad0884c5b1eeed452b0d6ad51d8339e0c78
Konstantin Popov <konstantin.icreative@gmail.com>
2016-08-26 18:06:36 +0600

    change controllers, add some auth and slash command

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,23 +4,12 @@ require 'curb'
 require 'json'
 
 class WelcomeController < ApplicationController
+  protect_from_forgery except: :verify
+
+  VERIFY_TOKEN = 'Dtu2szNL3sIXxJ950C3WSEwf'
+
   def index
-    if params[:code].present?
-      # byebug
-      code = params[:code]
-      http = Curl.post("https://slack.com/api/oauth.access", {client_id: '9390867412.72436541940', client_secret: 'bf4915d1940e75a8843b91fac64e7743', code: code})
-      puts http.body_str
-      parsed_json = JSON.parse(http.body_str)
-      # save token, team_id, team_name
-      integrate = Integration.new(access_token: parsed_json['access_token'], user_id: parsed_json['user_id'],
-                                  team_id: parsed_json['team_id'], team_name: parsed_json['team_name'],
-                                  channel: parsed_json['incoming_webhook']['channel'],
-                                  channel_id: parsed_json['incoming_webhook']['channel_id'],
-                                  url: parsed_json['incoming_webhook']['url'],
-                                  cofiguration_url: parsed_json['incoming_webhook']['configuration_url'])
-      integrate.save
-      #integrate.access_token = http.body_str()
-    elsif params[:hash_url].present?
+    if params[:hash_url].present?
       hash_url = params[:hash_url]
       @url = Link.find_by_hash_url(hash_url).url
       render 'welcome/view_video', url: @url
@@ -42,15 +31,24 @@ class WelcomeController < ApplicationController
 
       s3 = Aws::S3::Resource.new(region: 'us-west-2')
       s3_obj = s3.bucket('screencaster-purrweb').object("movies/#{link.hash_url}.mp4")
-      #s3_obj.upload_file("movies/#{link.hash_url}.mp4")
-      # link.update_attributes({url: s3_obj.public_url})
+      s3_obj.upload_file("movies/#{link.hash_url}.mp4")
+       link.update_attributes({url: s3_obj.public_url})
 
     end
     render json: {hash_url: link.hash_url}
   end
 
-  def start
+  def verify
+    if params[:token] == VERIFY_TOKEN
+      # byebug
+      response_url = params[:response_url]
+      Curl.post(response_url, { text: 'Are you ready to write video?', attachments: [ title: 'hello everybody',
+                                                                                      title_link: 'https://97735068.ngrok.io/welcome/start',
+                                                                                      text: 'what'] }.to_json)
+    end
 
+    redirect_to 'authentication/show'
+
+    #render nothing: true, status: 200
   end
 end
-# <%= progress_bar(33, :color => 'blue', :rounded => true) %>
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-
-  get 'welcome/start_page'
-  get 'welcome/start'
-
-
-  post 'welcome/video', to: 'welcome#video'
+  # root 'welcome#index'
+  #
+  root 'authentication#new'
+  resources :authentication, :welcome
+  post 'welcome/verify'
   get '/:hash_url', to: 'welcome#index', as: 'viewed_video'
-  post 'welcome/upload', to: 'welcome#upload'
 
-  resources :signed_url, only: :index
+  #
+  # get 'authentication/show'
+  #
+  # get 'welcome/start_page'
+  # get 'welcome/start'
+  # #get 'welcome/start'
+  #
+  # post 'welcome/verify'
+  #
+  # post 'welcome/video', to: 'welcome#video'
+  # post 'welcome/upload', to: 'welcome#upload'
+  # #post 'welcome/welcome/upload'
+  # resources :signed_url, only: :index
+  #
 
-
+  # root 'welcome/'
 end
