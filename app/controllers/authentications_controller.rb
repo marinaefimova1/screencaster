6c0581955566ab427456965c43424a6c90142dcc
Konstantin Popov <konstantin.icreative@gmail.com>
2016-08-29 12:52:39 +0600

    add authentacate

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -2,22 +2,20 @@ require 'rubygems'
 require 'streamio-ffmpeg'
 require 'curb'
 require 'json'
-class AuthenticationController < ApplicationController
-  def new
-    byebug
-    http = Curl.get("https://slack.com/oauth/authorize",{ client_id: '9390867412.72436541940', teams: 'read', users:'read'})
-    puts http.body_str
-    #parsed_json = JSON.parse(http.body_str)
-    # if Integration.where("access_token=?", parsed_json['access_token']).first
-    # redirect_to '/authentication/show'
-    # end
+class AuthenticationsController < ApplicationController
+
+  def show
   end
 
-  def index
+  def new
+  end
+
+  def authenticate
     if params[:code].present?
-      # byebug
       code = params[:code]
       http = Curl.get("https://slack.com/api/oauth.access", {client_id: '9390867412.72436541940', client_secret: 'bf4915d1940e75a8843b91fac64e7743', code: code})
+      # http.ssl_verify_peer = false
+      # http.perform
       parsed_json = JSON.parse(http.body_str)
       integrate = Integration.new(access_token: parsed_json['access_token'], user_id: parsed_json['user_id'],
                                   team_id: parsed_json['team_id'], team_name: parsed_json['team_name'],
@@ -27,9 +25,6 @@ class AuthenticationController < ApplicationController
                                   cofiguration_url: parsed_json['incoming_webhook']['configuration_url'])
       integrate.save
     end
-   redirect_to '/authentication/show'
-  end
-
-  def show
+    redirect_to authentications_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,17 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
-  #
-  root 'authentication#new'
-  resources :authentication, :welcome
-  post 'welcome/verify'
+  root 'authentications#new'
+
+  resource :authentications, only: [:show, :new] do
+    get :authenticate, on: :member
+  end
+  resources :welcome
   get '/:hash_url', to: 'welcome#index', as: 'viewed_video'
 
-  #
-  # get 'authentication/show'
-  #
   # get 'welcome/start_page'
   # get 'welcome/start'
-  # #get 'welcome/start'
-  #
   # post 'welcome/verify'
-  #
   # post 'welcome/video', to: 'welcome#video'
   # post 'welcome/upload', to: 'welcome#upload'
   # #post 'welcome/welcome/upload'
-  # resources :signed_url, only: :index
-  #
 
-  # root 'welcome/'
 end
